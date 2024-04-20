05f048d67d0e1cb325ddb330d0c7302899cd523d
ftios96 <167593919+ftios96@users.noreply.github.com>
2024-04-20 09:24:55 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,146 @@
+// function generateNumber() {
+//     for (let index = 0; index <= 30; index++) {
+
+//         var number = document.getElementById("randomNumber");
+//         number.textContent = Math.floor((Math.random() * 100) + 1);
+
+//     }
+
+// }
+
+console.log("Quang Huy - Chào mừng bạn đến giải thưởng quay số ngẫu nhiên")
+
+var num1 = document.querySelector("#num1");
+var num2 = document.querySelector("#num2");
+var number = document.querySelector("#randomNumber");
+let activeRolling = false;
+let timer;
+
+function generateRandom() {
+    setRightOrder(num1, num2);
+    if (activeRolling === false) {
+        var randomNumber = randomNum(num1, num2);
+        rollNumber(number.textContent, randomNumber);
+    } else {
+        activeRolling = false;
+        clearInterval(timer);
+        var randomNumber = randomNum(num1, num2);
+        rollNumber(number.textContent, randomNumber);
+        console.info("true", 1);
+    }
+}
+
+function randomNum(min, max) {
+    // var min = (parseInt(min.value));
+    // var max = (parseInt(max.value) + 1);
+    // var random = ((Math.floor(Math.random() * (max - min)) + min));
+    var random = Math.floor((Math.random() * 100) + 1)
+    return random;
+}
+
+function setRightOrder(num1, num2) {
+    const value1 = num1.value;
+    const value2 = num2.value;
+    if (value1 > value2) {
+        num1.value = value2;
+        num2.value = value1;
+    }
+}
+
+function rollNumber(number1, number2) {
+    start = parseInt(number1)
+    end = parseInt(number2);
+    var time = function(start, end) {
+        dif = Math.abs(start - end);
+        // console.info("start", start);
+        // console.info("end", end);
+        // console.info("dif", dif);
+
+        if (dif < 10) {
+            return 50;
+        } else if (dif < 20) {
+            return 40;
+        } else if (dif < 50) {
+            return 30;
+        } else if (dif < 100) {
+            return 20;
+        } else if (dif < 500) {
+            return 5;
+        } else if (dif < 1000) {
+            return 1;
+        }
+    }
+
+    if (activeRolling === false) {
+        activeRolling = true;
+        if (start < end) {
+            timer = setInterval(function() {
+                if (start == end) {
+                    clearInterval(timer);
+                    activeRolling = false;
+                }
+                var dif = (Math.abs(start - end))
+                if (dif > 100000) {
+                    start += (10000);
+                    number.textContent = start;
+                } else if (dif > 100000) {
+                    start += (10000);
+                    number.textContent = start;
+                } else if (dif > 10000) {
+                    start += (1000);
+                    number.textContent = start;
+                } else if (dif > 1000) {
+                    start += (100);
+                    number.textContent = start;
+                } else if (dif > 100) {
+                    start += (10);
+                    number.textContent = start;
+                } else {
+                    number.textContent = start++;
+                }
+                console.info("start", start);
+                console.info("textContent", 1);
+            }, time(start, end));
+        } else if (start > end) {
+            timer = setInterval(function() {
+                if (start == end) {
+                    clearInterval(timer);
+                    activeRolling = false;
+                }
+                var dif = (Math.abs(start - end))
+                if (dif > 100000) {
+                    start -= (10000);
+                    number.textContent = start;
+                } else if (dif > 100000) {
+                    start -= (10000);
+                    number.textContent = start;
+                } else if (dif > 10000) {
+                    start -= (1000);
+                    number.textContent = start;
+                } else if (dif > 1000) {
+                    start -= (100);
+                    number.textContent = start;
+                } else if (dif > 100) {
+                    start -= (10);
+                    number.textContent = start;
+                } else {
+                    number.textContent = start--;
+                }
+                console.info("textContent", 2);
+            }, time(start, end));
+        } else {
+            number.textContent = start;
+            activeRolling = false;
+            console.info("textContent", 3);
+        }
+    }
+}
+
+function setMax() {
+    if (parseInt(num1.value) > 100) {
+        num1.value = 100;
+    }
+    if (parseInt(num2.value) > 100) {
+        num2.value = 100;
+    }
+}
