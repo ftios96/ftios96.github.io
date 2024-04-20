dfe05bebcf2111ab748a5d622aa34387b34fe0bd
ftios96 <167593919+ftios96@users.noreply.github.com>
2024-04-20 11:04:40 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -144,3 +144,203 @@ function setMax() {
         num2.value = 100;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+$("#h1Toggle").on("dbclick", function() {
+    $(".ToggleShow").toggleClass("highlight");
+});
+
+function Chon3O() {
+    $('#chk2O').prop('checked', false);
+    if (document.getElementById('chk3O').checked) {
+        document.getElementById('dv3O').style.display = 'block';
+        document.getElementById('div2O').style.display = 'none';
+
+    } else {}
+}
+
+
+function Chon2O() {
+    $('#chk3O').prop('checked', false);
+    if (document.getElementById('chk2O').checked) {
+        document.getElementById('dv3O').style.display = 'none';
+        document.getElementById('div2O').style.display = 'block';
+    } else {}
+}
+
+
+function quay() {
+    var check2o = document.getElementById('chk2O').checked;
+    if (check2o == false) {
+
+        var somin = document.getElementById('true-random-integer-generator-min').value;
+        var somax = document.getElementById('true-random-integer-generator-max').value;
+        var soluongtoida = document.getElementById('SoLuongToiDa').value;
+        if (somin.trim() == '' || somax.trim() == '' || soluongtoida.trim() == '') {
+            alert('Hãy nhập đủ số');
+        } else {
+
+            var chuoisola = '';
+
+            if (document.getElementById('chk3O').checked) {
+                for (var j = parseInt(somin); j <= parseInt(somax); j++) {
+                    chuoisola += j + ",";
+                }
+            } else {}
+
+            var catra = chuoisola.replace(/^\|+,\|+$/g, '');
+
+            catra = catra.substring(0, catra.length - 1);
+            var arr = catra.split(',');
+
+            var chay = 0;
+
+            var gioihan = parseInt($('#slTocDo').val() + '0');
+
+            var rd = 0;
+
+            var audio = new Audio('QuaySo1.m4a');
+            audio.play();
+
+            var refreshIntervalId = setInterval(function() {
+
+                rd = Math.floor(Math.random() * arr.length);
+
+                if (chay == gioihan) {
+                    // $('#loadingnha').html(datas.d);
+                    audio.pause();
+                    var audio2 = new Audio('fanfare-winner.mp3');
+                    audio2.play();
+
+                    clearInterval(refreshIntervalId);
+                } else {
+                    chay += 1;
+                    $('#loadingnha').html(arr[rd]);
+                }
+
+                //  $('#loadingnha').html(chay);
+            }, 50);
+
+            //setTimeout(function () {
+            //    var str = '';
+
+            //    var chuoi = datas.d.toString().split('-');
+            //    var chay = 0;
+            //    for (var i = 0; i < chuoi.length; i++) {
+
+            //        if (chay == 5) {
+            //            chay = 0;
+            //            str += chuoi[i];
+            //            str += "<br>";
+            //        }
+            //        else {
+            //            str += chuoi[i];
+            //            str += "-";
+            //        }
+
+            //        chay += 1;
+            //    }
+
+            //    $('#loadingnha').html(datas.d);
+            //}, 2000);
+
+        }
+    } else {
+        if (check2o) {
+            var chuoi = $('#txtareadayso').val();
+            var soluongtoida = document.getElementById('SoLuongToiDa2').value;
+
+            var chuoisola = chuoi;
+
+            var gioihan = parseInt($('#slTocDo2').val() + '0');
+
+            var catra = chuoisola.replace(/^\|+,\|+$/g, '');
+
+            catra = catra.substring(0, catra.length - 1);
+            var arr = catra.split(',');
+
+            var chay = parseInt($('#slTocDo2').val());
+
+            var audio = new Audio('QuaySo1.m4a');
+            audio.play();
+
+            var rd = 0;
+            var refreshIntervalId = setInterval(function() {
+
+                rd = Math.floor(Math.random() * arr.length);
+
+                if (chay == gioihan) {
+                    // $('#loadingnha').html(datas.d);
+                    audio.pause();
+                    var audio2 = new Audio('fanfare-winner.mp3');
+                    audio2.play();
+
+                    clearInterval(refreshIntervalId);
+                } else {
+                    chay += 1;
+                    $('#loadingnha').html(arr[rd]);
+                }
+
+                //  $('#loadingnha').html(chay);
+            }, 50);
+
+            //setTimeout(function () {
+            //    $('#loadingnha').html(datas.d);
+            //}, 2000);
+        }
+    }
+    //if (datas.d.includes("Bạn")) {
+    //    alert(datas.d);
+    //    $('#loadingnha').html('Bấm nút quay để bắt đầu');
+    //}
+    //else {
+
+    //    var str = '';
+
+    //    var chuoi = datas.d.toString().split('-');
+    //    var chay = 0;
+    //    for (var i = 0; i < chuoi.length; i++) {
+
+    //        if (chay == 5) {
+    //            chay = 0;
+    //            str += chuoi[i];
+    //            str += "<br>";
+    //        }
+    //        else {
+    //            str += chuoi[i];
+    //            str += "-";
+    //        }
+
+    //        chay += 1;
+    //    }
+
+    //    $('#loadingnha').html(datas.d);
+    //}
+    // $('#loadingnha').html('Bấm nút quay để bắt đầu');
+
+
+
+}
+
+var htmlTocDo = "";
+for (var i = 1; i <= 20; i++) {
+
+    if (i == 3 || i == 5 || i == 8 || i == 10 || i == 15 || i == 20)
+        htmlTocDo += "<option value='" + i + "'>" + i + "</option>";
+    //slTocDo
+}
+
+$('#slTocDo').html(htmlTocDo);
+$('#slTocDo2').html(htmlTocDo);
+
+$('#slTocDo').val(5);
+$('#slTocDo2').val(5);
